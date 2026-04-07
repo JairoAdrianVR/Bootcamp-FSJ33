81d13405db64b62db98673ab3f6144b23148144e
JairoAdrianVR <jairoavegaromero@gmail.com>
2026-04-07 02:05:57 -0300

    Variables y constantes - Clase 6/4/2026

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,23 @@
+//alert("Holiwis");
+let variablecita = 'soy un string, cadena de texto para los amigos';
+ variablecita = "holiwis soy variablecita"
+
+let variblezota = "soy una variable grande" 
+
+let numerito = 1;
+// Se puede declarar varias variables con un solo let separandolas con la ,
+let string = "holiwis soy un string",  numerazo = 1235.345;
+
+// Imprimir
+console.log("Holiwis desde la consola");
+console.log(variablecita);
+console.log(variblezota);
+
+const pi = 3.1416;
+
+let variableNula = null;
+console.log(variableNula);
+
+let variableSinValor; 
+console.log(variableSinValor);
+
